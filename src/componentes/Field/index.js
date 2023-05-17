bdf97204719cb5480f607193b6e1e16b7a86e428
naylaprilla <nayla.priscila@gmail.com>
2023-05-17 09:11:41 -0300

    Add type Field

--- src/componentes/Field/index.js
+++ src/componentes/Field/index.js
@@ -1,11 +1,11 @@
 import './Field.css'
 
-const Field = ({label, placeholder, valor, whenChanged, mandatory = false}) => {
+const Field = ({type, label, placeholder, valor, whenChanged, mandatory = false}) => {
     
     return (
     <div className="field" >
         <label>{label}</label>
-        <input value={valor} onChange={evento => whenChanged(evento.target.value)} required={mandatory} placeholder={placeholder}/>
+        <input value={type} onChange={evento => whenChanged(evento.target.value)} required={mandatory} placeholder={placeholder}/>
     </div>
     )
 }
